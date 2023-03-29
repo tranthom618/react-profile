520ab4c99647b458dd4c92819bbcd88ca13d915c
Thomas Tran <tranthom618@gmail.com>
2023-03-29 04:02:49 -0400

    AboutMe/Homepage Added

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -1,8 +1,27 @@
 import React from 'react'
+import "../styles/AboutMe.css";
 
 function AboutMe() {
   return (
-    <h2>About Me</h2>
+    <div className = "home">
+      <div className = "about">
+        <h2> Hello! I'm Thomas!</h2>
+        <div className = "prompt">A new full stack developer, trying to learn everything there is to coding!</div>
+      </div>
+      <div className = "skills">
+        <h1>Skills</h1>
+        <ol className= "list">
+          <li className = "item">
+            <h2>Front-End</h2>
+            <span>ReactJS, HTML, CSS, NPM, BootStrap, MaterialUI, TailwindCSS</span>
+          </li>
+          <li className = "item">
+            <h2>Back-End</h2>
+            <span>NodeJS, JavaScript, ExpressJS, GraphQL, ApolloServer, MySQL, MongoDB</span>
+          </li>
+        </ol>
+      </div>
+    </div>
   )
 }
 
