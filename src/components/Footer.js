4a764d2365a89b825eafde85ad6a6ccea80a812b
Thomas Tran <tranthom618@gmail.com>
2023-03-29 03:51:21 -0400

    Footer with Social Media Icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,19 +1,20 @@
 import React from 'react'
-import { Link } from "react-router-dom";
 import "../styles/Footer.css";
 
+// Material Icons Package Imports - For Social Media Icons
 import LinkedInIcon from "@material-ui/icons/LinkedIn";
 import GitHubIcon from "@material-ui/icons/GitHub";
-// import StackOverflowIcon from "@material-ui/icons/St";
+import StackOverflowIcon from "@material-ui/icons/Layers";
 
 function Footer() {
   return (
     <div className="footer">
-        <div className="links">
-          <Link to ="github.com/tranthom618"><GitHubIcon /></Link>
-          <Link to ="www.linkedin.com/in/thomas-tran-750488246/"><LinkedInIcon /></Link>
-          {/* <Link to ="tranthom618@gmail.com"><StackOverflowIcon /></Link> */}
+        <div className="socialMedia">
+          <a href="https://github.com/tranthom618"><GitHubIcon /></a>
+          <a href="https://linkedin.com/in/thomas-tran-750488246/"><LinkedInIcon /></a>
+          <a href="https://stackoverflow.com/users/21516331/thomas-tran"><StackOverflowIcon /></a>
         </div>
+        <p> &copy; 2023 tranthom618@gmail.com</p>
       </div>
   )
 }
