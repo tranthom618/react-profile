8d2d091c1ba59b68cae14340ac569ad20873f428
Thomas Tran <tranthom618@gmail.com>
2023-03-30 03:07:10 -0400

    Resume Page Layout

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import cv from "../Thomas-Tran.pdf";
 
 function Resume() {
   return (
-    <div>Resume</div>
-  )
+    <div>
+      <h3>Resume Download Link</h3>
+      <button className="button">
+        <a className="button" href={cv} download="Thomas-Tran.pdf">
+          Download Resume
+        </a>
+      </button>
+      <h2>Front-End</h2>
+      <span>ReactJS, HTML, CSS, NPM, BootStrap, MaterialUI, TailwindCSS</span>
+      <h2>Back-End</h2>
+      <span>NodeJS, JavaScript, ExpressJS, GraphQL, ApolloServer, MySQL, MongoDB</span>
+    </div>
+  );
 }
 
-export default Resume
+export default Resume;
