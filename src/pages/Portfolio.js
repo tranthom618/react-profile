d33a97207787ab056dde22a26c728d3cf282678c
Thomas Tran <tranthom618@gmail.com>
2023-04-03 10:27:04 -0400

    Project Added for Group 3 Project

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
+import "../styles/Portfolio.css";
+import ProjectPic1 from "../ProjectPic1.png";
 
 function Portfolio() {
   return (
-    <div>Portfolio</div>
-  )
+    <div>
+      <a href="https://oracle-online.herokuapp.com/">
+        <img src={ProjectPic1} alt="ProjectPicture1" />
+      </a>
+      <figcaption>Oracle (Full MERN Stack Collaboration Project)</figcaption>
+    </div>
+  );
 }
 
-export default Portfolio
+export default Portfolio;
